79cb4ed3f7b4635f74460ed02232fddf3eaa56a3
carolinedreis <caroline.reissp@gmail.com>
2019-12-11 10:29:02 -0300

    exercicios-aula-logica

---
+++ numero-par-impar.py
@@ -0,0 +1,7 @@
+numero = int ( input ( ' Entre com um número inteiro: ' ))
+
+if numero % 2 == 0 : 
+print ( ' O número ' , número, ' é par ' )
+
+else :
+ print ( ' O número ' , número, ' é ímpar ' )
